a25e15666a3d2f5ac23090a5a599e26fab5fc6f5
tdeugdoer <tegor2003@gmail.com>
2024-11-16 22:37:47 +0300

    Added discovery service

--- catalog-service/src/main/java/com/tserashkevich/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/tserashkevich/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.tserashkevich.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
--- doctor-service/src/main/java/com/tserashkevich/doctorservice/DoctorServiceApplication.java
+++ doctor-service/src/main/java/com/tserashkevich/doctorservice/DoctorServiceApplication.java
@@ -2,8 +2,10 @@ package com.tserashkevich.doctorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DoctorServiceApplication {
 
     public static void main(String[] args) {
