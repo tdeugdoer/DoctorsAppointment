f9fa9f90f9140e28eb0cab4ed6bd2a9a668bca85
tdeugdoer <tegor2003@gmail.com>
2025-02-05 18:24:55 +0300

    Deleted discovery and gateway services

--- feedback-service/src/main/java/com/tserashkevich/feedbackservice/FeedbackServiceApplication.java
+++ feedback-service/src/main/java/com/tserashkevich/feedbackservice/FeedbackServiceApplication.java
@@ -2,12 +2,10 @@ package com.tserashkevich.feedbackservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableEurekaServer
 @EnableFeignClients
 @EnableRetry
 public class FeedbackServiceApplication {
